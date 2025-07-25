7c4317783e93edcd2ad9b65bc90b037a91634e33
Siddhesh <43413208+candobettercode@users.noreply.github.com>
2025-07-25 23:03:45 +0530

    model notebook added

--- demo.py
+++ demo.py
@@ -7,12 +7,12 @@ from src.logger import logging
 #logging.critical("This is a critical message.")
 
 # below code is to check the exception config
-from src.logger import logging
-from src.exception import MyException
-import sys
+#from src.logger import logging
+#from src.exception import MyException
+#import sys
 
-try:
-    a = 1+'Z'
-except Exception as e:
-    logging.info(e)
-    raise MyException(e, sys) from e
+#try:
+#    a = 1+'Z'
+#except Exception as e:
+#    logging.info(e)
+#    raise MyException(e, sys) from e
